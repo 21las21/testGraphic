15c2045ec8dd3234f5ecad528c82fc0805122ede
21las21 <sasha.kudasov04@gmail.com>
2015-09-05 09:16:00 +0500

    Graphics

--- testGraphic.py
+++ testGraphic.py
@@ -2,15 +2,30 @@ import pygame
 
 def render():
 
-    screen.fill((0, 0, 0))
+    screen.fill((0, 255, 0))
 
-    pygame.draw.line(screen,(255, 255, 255), (10,10), (630,10))
+    pygame.draw.line(screen,(255, 255, 255), (10,600), (300,600), (5))
 
-    pygame.draw.line(screen,(0, 0, 255), (10,10), (10,630))
+    pygame.draw.line(screen,(255, 255, 255), (300,600), (300,350), (5))
 
-    pygame.draw.line(screen,(0,255,0), (10,630), (630,630))
+    pygame.draw.line(screen,(255,255,255), (300,350), (10,350), (5))
 
-    pygame.draw.line(screen,(255,0,0), (630,630), (630,10))
+    pygame.draw.line(screen,(255,255,255), (10,350), (10,600), (5))
+
+    pygame.draw.line(screen,(255,255,255), (10,350), (155,300), (5))
+
+    pygame.draw.line(screen,(255,255,255), (155,300), (300,350), (5))
+
+    pygame.draw.circle(screen,(255,255,0), (100,100), 50)
+
+    pygame.draw.rect(screen,(0,0,255), ((305,450), (340,150)))
+
+    x = 325
+
+    while x < 640:
+        pygame.draw.line(screen,(0,0,0), (x,450), (x,600), 2)
+
+        x = x + 20
 
 screen = pygame.display.set_mode((640, 640))
 
